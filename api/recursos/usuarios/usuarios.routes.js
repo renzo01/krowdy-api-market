44564820805b0e772f903963e002ca2965144e49
renzo01 <renzogomez577@gmail.com>
2019-08-22 23:11:17 -0500

    more loggers

--- api/recursos/usuarios/usuarios.routes.js
+++ api/recursos/usuarios/usuarios.routes.js
@@ -8,22 +8,27 @@ const usuarioController = require('./usuarios.controller');
 
 const validateUsuario = require('./usuarios.validate');
 const usuarios = require('../../../db').usuarios;
+const logger = require('../../utils/logger');
 const usuariosRoutes = express.Router();
 
-usuariosRoutes.get('/', async (req,resp) => {
-  const usuarios = await usuarioController.obtenerUsuario();
-  resp.json(usuarios);
+
+usuariosRoutes.get('/', async (req, res) => {
+  const usuarios = await usuarioController.obtenerUsuarios();
+  logger.info(`Se hizo la peticion de mostrar los usuarios`);
+  res.json(usuarios);
 });
 
-usuariosRoutes.post('/', validateUsuario, async (req, resp) => {
-  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) =>{
-    if (err){
-      resp.status(500).send(`A ocurrido un error en el servidor con bcrypt`);
+usuariosRoutes.post('/', validateUsuario, async (req, res) => {
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    if (err) {
+      // logger.info(error)
+      logger.error(`ha ocurrido un error con la encriptacion`);
+      res.status(500).send(`A ocurrido un error en el servidor con bcrypt`);
       return
     }
-    const newUser = {...req.body, password: hashedPassword, id: uuidv4()};
-    const usuarios = require('../../../db').usuarios;
-    resp.status(201).send(`El usuario fue creado con exito`);
+    const newUser = { ...req.body, password: hashedPassword, id: uuidv4() };
+    const usuario = await usuarioController.crearUsuario(newUser);
+    res.status(201).send(`El usuario fue creado con exito.`);
   });
 
 });
@@ -32,15 +37,17 @@ usuariosRoutes.post('/login', validateUsuario, async (req, resp) =>{
   const usuario = await usuarioController.obtenerUsuario(req.body.username)
 
   if (!usuario) {
-    resp.status(201).send(`El usuario no existe. Verifica tu informacion`);
-    return
+    logger.info(`No se ha encontrado al usuario solicitado`);
+    res.status(404).send(`El usuario no existe. Verifica tu informacion.`);
+    return;
   }
-  //when the user is founded
-  bcrypt.compare(req.body.password, usuario.password, (err, coinside) =>{
-    if(err) {
-        console.log(err);
-        resp.status(500).send(`Algo ocurrio`);
-        return;
+  
+  bcrypt.compare(req.body.password, usuario.password, (err, coincide) => {
+    if (err) {
+      console.log(err);
+      logger.error(`La contraseña no coinside`);
+      res.status(500).send(`Algo ocurrio ups!`);
+      return;
     }
     if(coinside){
       const token = jwt.sign({id: usuario['__id']}, 'secreto', {expiresIn: 86400});
